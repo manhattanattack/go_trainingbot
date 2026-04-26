f5fa89f057ad817a04ab1c9c73e28e289ff7b100
manhattanattack <xxxxxaocccc@gmail.com>
2026-04-26 23:05:36 +0300

    forming response json in /me handler

--- main.go
+++ main.go
@@ -25,20 +25,22 @@ type contextKey string
 const userIDKey contextKey = "userID"
 
 type TrainingData struct {
-	Exercises []ExerciseData `json:"exercises"`
-	Date      string         `json:"date"`
+	TrainingId int            `json:"trainingId,omitempty"`
+	Exercises  []ExerciseData `json:"exercises"`
+	Date       string         `json:"date"`
 }
 
 type ExerciseData struct {
-	// ExcerciseId  uint8     `json:"exerciseId"`
-	BaseExercise uint8     `json:"baseExercise"`
+	ExerciseId   int       `json:"exerciseId,omitempty"`
+	BaseExercise int       `json:"baseExercise"`
 	Sets         []SetData `json:"sets"`
 }
 
 type SetData struct {
+	SetId  int     `json:"setId,omitempty"`
 	Weight float32 `json:"weight"`
-	Reps   uint8   `json:"reps"`
-	Rpe    uint8   `json:"rpe"`
+	Reps   int     `json:"reps"`
+	Rpe    int     `json:"rpe"`
 	Note   string  `json:"note,omitempty"`
 }
 
@@ -72,7 +74,7 @@ func insertTrainingData(ctx context.Context, trainingData TrainingData) error {
 	// if err != nil {
 	// 	return err
 	// }
-	result, err := tx.ExecContext(ctx, "INSERT INTO trainings (user_id, date) VALUES (?, ?)", 1, trainingData.Date)
+	result, err := tx.ExecContext(ctx, "INSERT INTO trainings (user_id, date) VALUES (?, ?)", ctx.Value(userIDKey), trainingData.Date)
 	if err != nil {
 		return err
 	}
@@ -83,18 +85,15 @@ func insertTrainingData(ctx context.Context, trainingData TrainingData) error {
 	for _, exercise := range trainingData.Exercises {
 		result, err = tx.ExecContext(ctx, "INSERT INTO exercises (base_exercise, training_id) VALUES (?, ?)", exercise.BaseExercise, trainingId)
 		if err != nil {
-			tx.Rollback()
 			return err
 		}
 		exerciseId, err := result.LastInsertId()
 		if err != nil {
-			tx.Rollback()
 			return err
 		}
 		for _, set := range exercise.Sets {
 			_, err = tx.ExecContext(ctx, "INSERT INTO sets (exercise_id, reps, weight, rpe, note) VALUES (?, ?, ?, ?, ?)", exerciseId, set.Reps, set.Weight, set.Rpe, set.Note)
 			if err != nil {
-				tx.Rollback()
 				return err
 			}
 		}
@@ -106,6 +105,15 @@ func insertTrainingData(ctx context.Context, trainingData TrainingData) error {
 	return nil
 }
 
+func sendResponse(w http.ResponseWriter, statusCode int, data map[string]string) {
+	encoder := json.NewEncoder(w)
+	w.WriteHeader(int(statusCode))
+	err := encoder.Encode(data)
+	if err != nil {
+		fmt.Printf("Не смог записать в json %v\n", err)
+	}
+}
+
 // мидлвейр выполняет передаваемую функцию next, но с модифицированным запросом
 // (добавляет к нему контекст с userId пользователя, и хендлер уже знает его и уверен что пользователь залогинен)
 func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
@@ -113,50 +121,30 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	// миддлвейр в свою очередь сразу же возвращает анонимную функцию, внутри которой проходит валидация и
 	// вызывается мой хендлер с модифицированным запросом, которую позже вызовет HandlerFunc когда придет запрос на роут
 	return func(w http.ResponseWriter, r *http.Request) {
-		data := map[string]any{
-			"error": "",
+		responseData := map[string]string{
+			"error": "Invalid authorization token",
 		}
-		encoder := json.NewEncoder(w)
 
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			w.WriteHeader(http.StatusUnauthorized)
-			data["error"] = "Authorization header is empty"
-			err := encoder.Encode(data)
-			if err != nil {
-				fmt.Printf("Не смог записать в json %v\n", err)
-			}
+			sendResponse(w, http.StatusUnauthorized, responseData)
 			return
 		}
 		authParts := strings.Split(authHeader, " ")
 		if len(authParts) != 2 {
-			w.WriteHeader(http.StatusUnauthorized)
-			data["error"] = "Authorization header is invalid"
-			err := encoder.Encode(data)
-			if err != nil {
-				fmt.Printf("Не смог записать в json %v\n", err)
-			}
+			sendResponse(w, http.StatusUnauthorized, responseData)
 			return
 		}
 		authType := authParts[0]
 		authData := authParts[1]
 		if authType != "tma" {
-			w.WriteHeader(http.StatusUnauthorized)
-			data["error"] = "Authorization header is invalid"
-			err := encoder.Encode(data)
-			if err != nil {
-				fmt.Printf("Не смог записать в json %v\n", err)
-			}
+			sendResponse(w, http.StatusUnauthorized, responseData)
 			return
 		}
 		userId, err := validateAndGetUserId(authData)
 		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			data["error"] = err
-			err := encoder.Encode(data)
-			if err != nil {
-				fmt.Printf("Не смог записать в json %v\n", err)
-			}
+			responseData["error"] = err.Error()
+			sendResponse(w, http.StatusUnauthorized, responseData)
 			return
 		}
 		ctx := context.WithValue(r.Context(), userIDKey, userId)
@@ -165,13 +153,11 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+// TODO: валидация json: чтобы нельзя было отправить 999 повторений и тд. можно использовать http.MaxBytesReader чтбоы не читать больше определенного объема
 func addTrainingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Получил запрос")
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	data := map[string]string{
+
+	responseData := map[string]string{
 		"status": "ok",
 	}
 	trainingData := TrainingData{}
@@ -182,9 +168,8 @@ func addTrainingHandler(w http.ResponseWriter, r *http.Request) {
 	err := decoder.Decode(&trainingData)
 	if err != nil {
 		fmt.Printf("Не смог прочитать из json %v\n", err)
-		data["status"] = "failed"
-		w.WriteHeader(http.StatusBadRequest)
-		encoder.Encode(data)
+		responseData["status"] = "failed"
+		sendResponse(w, http.StatusBadRequest, responseData)
 		return
 	}
 	fmt.Printf("Успешно распарсил: %+v\n", trainingData)
@@ -193,15 +178,89 @@ func addTrainingHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 	w.WriteHeader(http.StatusCreated)
-	err = encoder.Encode(data)
+	err = encoder.Encode(responseData)
 	if err != nil {
 		fmt.Printf("Не смог записать в json %v\n", err)
 	}
 }
 
-// func meHandler(w http.ResponseWriter, r *http.Request) {
+func meHandler(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(userIDKey)
+	_, err := db.Exec("INSERT OR IGNORE INTO users (user_id) VALUES (?)", userId)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	rows, err := db.Query(`
+	SELECT 
+		t.training_id AS t_id, t.date,
+		e.exercise_id AS e_id, e.base_exercise,
+		s.set_id AS s_id, s.reps, s.weight, s.rpe, s.note
+	FROM trainings t
+	LEFT JOIN exercises e ON t.training_id = e.training_id
+	LEFT JOIN sets s ON e.exercise_id = s.exercise_id
+	WHERE t.user_id = ?
+	ORDER BY t.date DESC; 
+	`, userId)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer rows.Close()
 
-// }
+	trainingsMap := make(map[any]*TrainingData)
+	exercisesMap := make(map[any]*ExerciseData)
+	var finalResponse []*TrainingData
+	for rows.Next() {
+		var training *TrainingData
+		var exercise *ExerciseData
+		var trainingId int
+		var date string
+		var exerciseId *int
+		var baseExercise *int
+		var setId *int
+		var reps *int
+		var weight *float32
+		var rpe *int
+		var note *string
+		if err := rows.Scan(&trainingId, &date, &exerciseId, &baseExercise, &setId, &reps, &weight, &rpe, &note); err != nil {
+			log.Fatal(err)
+		}
+		_, exists := trainingsMap[trainingId]
+		if !exists {
+			training = &TrainingData{
+				TrainingId: trainingId,
+				Date:       date,
+				Exercises:  make([]ExerciseData, 0),
+			}
+			trainingsMap[trainingId] = training
+			finalResponse = append(finalResponse, training)
+		}
+		if exerciseId != nil {
+			_, exists := exercisesMap[exerciseId]
+			if !exists {
+				exercise = &ExerciseData{
+					ExerciseId:   *exerciseId,
+					BaseExercise: *baseExercise,
+					Sets:         make([]SetData, 0),
+				}
+				exercisesMap[exerciseId] = exercise
+				training.Exercises = append(training.Exercises, *exercise)
+			}
+		}
+		if setId != nil {
+			newSet := SetData{
+				SetId:  *setId,
+				Weight: *weight,
+				Reps:   *reps,
+				Rpe:    *rpe,
+				Note:   *note,
+			}
+			exercise.Sets = append(exercise.Sets, newSet)
+		}
+		json.NewEncoder(w).Encode(finalResponse)
+	}
+}
 
 // func profileHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -211,7 +270,7 @@ func createTables() {
 	query := `
 	CREATE TABLE IF NOT EXISTS users (
 		user_id INTEGER PRIMARY KEY,
-		name TEXT NOT NULL,
+		name TEXT,
 		weight REAL,
 		height INTEGER 
 	);
@@ -224,7 +283,7 @@ func createTables() {
 	);
 
 	CREATE TABLE IF NOT EXISTS base_exercises (
-		base_id INTEGER  PRIMARY KEY ,
+		base_id INTEGER PRIMARY KEY,
 		description TEXT
 	);
 
@@ -272,7 +331,8 @@ func initDB() {
 func main() {
 	initDB()
 	http.Handle("/", http.FileServer(http.Dir("static")))
-	http.HandleFunc("/api/training", authMiddleware(addTrainingHandler))
+	http.HandleFunc("POST /api/training", authMiddleware(addTrainingHandler))
+	http.HandleFunc("GET /me", authMiddleware(meHandler))
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
